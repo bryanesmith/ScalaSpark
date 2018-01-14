34a91cf42a68ec920103e42dad863e0c326d9bff
Bryan Smith <bryanesmith@gmail.com>
2018-01-14 13:39:25 -0500

    add AmountPerCustomer

---
+++ src/main/scala/com/bryanesmith/spark/section3/AmountPerCustomer.scala
@@ -0,0 +1,30 @@
+package com.bryanesmith.spark.section3
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.SparkContext
+
+import com.bryanesmith.spark.SparkContextExtras._
+
+object AmountPerCustomer {
+
+  // Format: customer id, product id, amount
+  private def parse(line: String) = {
+    val fields = line.split(",")
+    (fields(0).toInt, fields(2).toFloat)
+  }
+
+  def main(args: Array[String]): Unit = {
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    val sc = new SparkContext("local", getClass.getSimpleName)
+
+    sc.resourceTextFile("/section3/customer-orders.csv")
+      .map(parse)                 // [ (customer, amount) ]
+      .reduceByKey { _ + _ }      // [ (customer, total amount) ]
+      .map(e => (e._2, e._1))
+      .sortByKey()
+      .foreach { e =>
+        println(f"${e._2}%05d => $$${e._1}%.2f")
+      }
+  }
+}
