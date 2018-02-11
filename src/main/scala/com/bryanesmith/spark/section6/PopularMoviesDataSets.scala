a91a0532581138ca1e2c09f86f91e00ce3208290
Bryan Smith <bryanesmith@gmail.com>
2018-02-11 17:39:50 -0500

    PopularMoviesDataSets.scala cleanup

--- src/main/scala/com/bryanesmith/spark/section6/PopularMoviesDataSets.scala
+++ src/main/scala/com/bryanesmith/spark/section6/PopularMoviesDataSets.scala
@@ -12,77 +12,64 @@ import scala.io.{Codec, Source}
 
 /** Find the movies with the most ratings. */
 object PopularMoviesDataSets {
-  
+
   /** Load up a Map of movie IDs to movie names. */
   def loadMovieNames() : Map[Int, String] = {
-    
-    // Handle character encoding issues:
+
     implicit val codec: Codec = Codec("UTF-8")
     codec.onMalformedInput(CodingErrorAction.REPLACE)
     codec.onUnmappableCharacter(CodingErrorAction.REPLACE)
 
-    // Create a Map of Ints to Strings, and populate it from u.item.
-    var movieNames:Map[Int, String] = Map()
-
     val path = IOUtils.resourceFilePath("/section1/ml-100k/u.item")
-    
-     val lines = Source.fromFile(path).getLines
-     for (line <- lines) {
-       val fields = line.split('|')
-       if (fields.length > 1) {
-        movieNames += (fields(0).toInt -> fields(1))
-       }
-     }
-    
-     movieNames
+
+    Source.fromFile(path).getLines
+      .map { _.split('|') }
+      .filter { _.length > 1 }
+      .map { fields => (fields(0).toInt, fields(1)) }
+      .toMap
   }
- 
-  // Case class so we can get a column name for our movie ID
+
   final case class Movie(movieID: Int)
-  
-  /** Our main function where the action happens */
+
   def main(args: Array[String]) {
-   
-    // Set the log level to only print errors
+
     Logger.getLogger("org").setLevel(Level.ERROR)
-    
-    // Use new SparkSession interface in Spark 2.0
+
     val spark = SparkSession
       .builder
       .appName("PopularMovies")
       .master("local[*]")
-      .config("spark.sql.warehouse.dir", "file:///C:/temp") // Necessary to work around a Windows bug in Spark 2.0.0; omit if you're not on Windows.
       .getOrCreate()
-    
-    // Read in each rating line and extract the movie ID; construct an RDD of Movie objects.
+
     val lines = spark.sparkContext
       .resourceTextFile("/section1/ml-100k/u.data")
-      .map(x => Movie(x.split("\t")(1).toInt))
-    
-    // Convert to a DataSet
+      .map { line =>
+        val tokens = line.split("\t")
+        Movie(tokens(1).toInt)
+      }
+
     import spark.implicits._
     val moviesDS = lines.toDS()
-    
-    // Some SQL-style magic to sort all movies by popularity in one line!
+
     val topMovieIDs = moviesDS.groupBy("movieID").count().orderBy(desc("count")).cache()
-    
+
     // Show the results at this point:
     /*
     |movieID|count|
     +-------+-----+
     |     50|  584|
     |    258|  509|
-    |    100|  508|   
+    |    100|  508|
     */
-    
+
     topMovieIDs.show()
-    
+
     // Grab the top 10
     val top10 = topMovieIDs.take(10)
-    
+
     // Load up the movie ID -> name map
     val names = loadMovieNames()
-    
+
     // Print the results
     println
     for (result <- top10) {
@@ -94,6 +81,5 @@ object PopularMoviesDataSets {
     // Stop the session
     spark.stop()
   }
-  
-}
 
+}
