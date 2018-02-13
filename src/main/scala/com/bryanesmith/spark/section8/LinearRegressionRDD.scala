629437c2359351569f0fd6a9ae8c07d2405ac368
Bryan Smith <bryanesmith@gmail.com>
2018-02-13 08:38:00 -0500

    LinearRegression -> LinearRegressionRDD, with cleanup

---
+++ src/main/scala/com/bryanesmith/spark/section8/LinearRegressionRDD.scala
@@ -0,0 +1,39 @@
+package com.bryanesmith.spark.section8
+
+import com.bryanesmith.spark.SparkContextExtras._
+import org.apache.log4j._
+import org.apache.spark._
+import org.apache.spark.mllib.optimization.SquaredL2Updater
+import org.apache.spark.mllib.regression.{LabeledPoint, LinearRegressionWithSGD}
+
+object LinearRegressionRDD {
+
+  def main(args: Array[String]) {
+
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    val sc = new SparkContext("local[*]", "LinearRegression")
+
+    // x,y number pairs where x is the label, and y is the feature.
+    // Note that the data file contains values scaled to -1 to 1 with mean of 0.
+    val allData = sc.resourceTextFile("/section8/regression.txt")
+      .randomSplit(Array(0.8, 0.2))
+
+    val trainingData = allData(0)
+      .map(LabeledPoint.parse).cache()
+
+    val testData = allData(1).map(LabeledPoint.parse)
+
+    val algorithm = new LinearRegressionWithSGD()
+    algorithm.optimizer
+      .setNumIterations(100)
+      .setStepSize(1.0)
+      .setUpdater(new SquaredL2Updater())
+      .setRegParam(0.01)
+
+    algorithm.run(trainingData)
+      .predict(testData.map { _.features })
+      .zip(testData.map { _.label })
+      .foreach(println)
+  }
+}
