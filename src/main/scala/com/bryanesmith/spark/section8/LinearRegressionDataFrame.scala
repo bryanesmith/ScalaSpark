6b7e39c296537996537a2c21bf222250e9e05176
Bryan Smith <bryanesmith@gmail.com>
2018-02-13 08:55:54 -0500

    LinearRegressionDataFrame

---
+++ src/main/scala/com/bryanesmith/spark/section8/LinearRegressionDataFrame.scala
@@ -0,0 +1,43 @@
+package com.bryanesmith.spark.section8
+
+import com.bryanesmith.spark.SparkContextExtras._
+import org.apache.log4j._
+import org.apache.spark.ml.linalg.Vectors
+import org.apache.spark.ml.regression.LinearRegression
+import org.apache.spark.sql._
+
+object LinearRegressionDataFrame {
+
+  def main(args: Array[String]) {
+
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    val spark = SparkSession
+      .builder
+      .appName("LinearRegressionDF")
+      .master("local[*]")
+      .getOrCreate()
+
+    val inputLines = spark.sparkContext.resourceTextFile("/section8/regression.txt")
+
+    val data = inputLines.map{ _.split(",") }
+      .map { x => (x(0).toDouble, Vectors.dense { x(1).toDouble }) }
+
+    import spark.implicits._
+    val trainTest = data.toDF("label", "features")
+      .randomSplit(Array(0.8, 0.2))
+
+    new LinearRegression()
+      .setRegParam(0.3)
+      .setElasticNetParam(0.8)
+      .setMaxIter(100)
+      .setTol(1E-6)
+      .fit(trainTest(0))
+      .transform(trainTest(1)).cache()
+      .select("prediction", "label").rdd
+      .map { x => (x.getDouble(0), x.getDouble(1)) }
+      .foreach(println)
+
+    spark.stop()
+  }
+}
